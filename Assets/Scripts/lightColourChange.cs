459ed880f1337128f3bc1eba6fac8f41e3c6751d
Erkki <erkki@hellopupil.com>
2018-08-03 08:23:23 +0100

    First simplification
    Checked that app builds and runs

---
+++ Assets/Scripts/lightColourChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightColourChange : MonoBehaviour {
+
+	//public Color color1 = Color.red;
+    //public Color color2 = Color.blue;
+    //public float duration = 10.0F;
+
+    public Gradient myGradient;
+    public float strobeDuration = 100F;
+
+    public Light light;
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        //float t = Mathf.PingPong(Time.time, duration) / duration;
+        //cam.backgroundColor = Color.Lerp(color1, color2, t);
+
+        float t = Mathf.PingPong(Time.time, strobeDuration) / strobeDuration;
+        light.color = myGradient.Evaluate(t);
+    }
+}
